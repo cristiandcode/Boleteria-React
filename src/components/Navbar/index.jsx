da8b663165b93e69f87f1a00423cdda5f4820ff7
Cristian Diaz <diazcristian0210@gmail.com>
2024-11-26 17:44:12 -0300

    Agregue un estado al input con useState

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,21 @@
-const Navbar = () =>{
-    return(
-    <div>
-        <p>Mi boletera</p>
-        <input placeholder="Busca tu evento favorito"></input>
-    </div>
+import { useState } from "react";
 
-    )
+const Navbar = () => {
+  const [search, setSearch] = useState('');
+  const handleInputChange = (evt) => {
+    setSearch(evt.target.value);
+  };
+
+  return (
+    <div>
+      <p>Mi boletera</p>
+      <input
+        placeholder="Busca tu evento favorito"
+        onChange={handleInputChange}
+        value={search}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
